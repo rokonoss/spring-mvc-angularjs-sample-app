5f7c7fb5db86c200f7421285ff9398dc05e46976
Muhammad Rukunuzzaman <rokon.3422@gmail.com>
2016-03-15 09:47:20 +1100

    added RUN.sh

---
+++ RUN.sh
@@ -0,0 +1,3 @@
+cd "D:\JavaProjectsHome\Spring-Angular\spring-mvc-angularjs-sample-app"
+export JAVA_HOME="C:\Program Files\Java\jdk1.8.0_11"
+mvn clean install tomcat7:run-war -Dspring.profiles.active=test
